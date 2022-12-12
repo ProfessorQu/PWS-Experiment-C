fbe2170dafe6e105a4e673b2e78d885d9550b375
ProfessorQu <quint@dasookwat.com>
2022-12-12 21:45:48 +0100

    Moved definitions, Added erasing, Added restarting, Update grid left to right and then switch every frame, Made SandReact more general

--- main.c
+++ main.c
@@ -1,60 +1,22 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic 3d example
-*
-*   Welcome to raylib!
-*
-*   To compile example, just press F5.
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2022 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 #include "raylib.h"
 
 //----------------------------------------------------------------------------------
-// Local Variables Definition (local to this module)
+// Global Variables Definition
 //----------------------------------------------------------------------------------
+typedef enum {
+    Empty, Water, Sand, Product
+} CellType;
+
 #define screenWidth 800
 #define screenHeight 800
 
-#define cellSize 2
+#define cellSize 5
 
 #define gridWidth (screenWidth / cellSize)
 #define gridHeight (screenHeight / cellSize)
-
-const int gravity = 3;
-
-const int waterSpreadRate = 10;
-
-enum CellType { Empty, Water, Sand, Product };
-
-enum CellType grid[gridWidth][gridHeight];
-
-bool InBounds(int x, int y)
-{
-    return 0 <= x && x < gridWidth && 0 <= y && y < gridHeight;
-}
-
-bool swap(enum CellType *a, enum CellType *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-
-    return true;
-}
 
 //----------------------------------------------------------------------------------
 // Local Functions Declaration
@@ -62,18 +24,48 @@ bool swap(enum CellType *a, enum CellType *b)
 static void Update(void);
 static void Draw(void);
 
+void UpdateGridLeft();
+void UpdateGridRight();
+
 bool Fall(int x, int y);
-bool FallIn(int x, int y, enum CellType type);
+bool FallIn(int x, int y, CellType type);
+
+void UpdateEmpty(int x, int y);
 
 void UpdateWater(int x, int y);
 bool WaterSpread(int x, int y);
 
 void UpdateSand(int x, int y);
-bool SandReact(int x, int y);
+bool React(int x, int y, CellType with, CellType to);
 
 void UpdateProduct(int x, int y);
 
-void SpawnCells(int size, enum CellType type);
+void SpawnCells(int size, CellType type);
+
+//----------------------------------------------------------------------------------
+// Local Variables Definition (local to this module)
+//----------------------------------------------------------------------------------
+const int gravity = 3;
+
+const int waterSpreadRate = 5;
+
+bool left = false;
+
+CellType grid[gridWidth][gridHeight];
+
+bool InBounds(int x, int y)
+{
+    return 0 <= x && x < gridWidth && 0 <= y && y < gridHeight;
+}
+
+bool swap(CellType *a, CellType *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+
+    return true;
+}
 
 //----------------------------------------------------------------------------------
 // Main entry point
@@ -103,14 +95,44 @@ int main()
     return 0;
 }
 
+//----------------------------------------------------------------------------------
+// Update everything
+//----------------------------------------------------------------------------------
 void Update()
 {
+    // Spawning Cells
+    //---------------------------------------------------------
     if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
         SpawnCells(10, Water);
     else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
         SpawnCells(5, Sand);
+    else if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))
+        SpawnCells(10, Empty);
     
+    // Reset Grid
+    //---------------------------------------------------------
+    if (IsKeyPressed(KEY_R))
+        for (int x = 0; x < gridWidth; x++)
+            for (int y = 0; y < gridHeight; y++)
+                grid[x][y] = Empty;
+        
+    // Update Grid
+    //---------------------------------------------------------
+    if (left)
+        UpdateGridLeft();
+    else
+        UpdateGridRight();
+    
+    left = !left;
+}
+
+//----------------------------------------------------------------------------------
+// Update grid left to right
+//----------------------------------------------------------------------------------
+void UpdateGridLeft()
+{
     for (int y = gridHeight - 1; y >= 0 ; y--)
+    {
         for (int x = 0; x < gridWidth; x++)
         {
             if (grid[x][y] == Water)
@@ -120,13 +142,35 @@ void Update()
             else if (grid[x][y] == Product)
                 UpdateProduct(x, y);
         }
+    }
 }
 
-void SpawnCells(int size, enum CellType type)
+//----------------------------------------------------------------------------------
+// Update grid right to left
+//----------------------------------------------------------------------------------
+void UpdateGridRight()
 {
+    for (int y = gridHeight - 1; y >= 0 ; y--)
+    {
+        for (int x = gridWidth - 1; x >= 0; x--)
+        {
+            if (grid[x][y] == Water)
+                UpdateWater(x, y);
+            else if (grid[x][y] == Sand)
+                UpdateSand(x, y);
+            else if (grid[x][y] == Product)
+                UpdateProduct(x, y);
+        }
+    }
+}
+
+void SpawnCells(int size, CellType type)
+{
+    // Get the mouse position
     int mouseX = GetMouseX() / cellSize;
     int mouseY = GetMouseY() / cellSize;
 
+    // Set cells in a (2*size)*(2*size) square
     for (int cx = -size; cx <= size; cx++)
         for (int cy = -size; cy <= size; cy++)
             if (InBounds(mouseX + cx, mouseY + cy))
@@ -143,7 +187,7 @@ bool WaterSpread(int x, int y)
 {
     for (int distance = waterSpreadRate; distance > 0; distance--)
     {
-        int randomDirection = rand() % 2 - 1;
+        int randomDirection = rand() % 3 - 1;
         int offset = distance * randomDirection;
 
         if (InBounds(x + offset, y) && grid[x + offset][y] == Empty)
@@ -157,27 +201,9 @@ bool WaterSpread(int x, int y)
 
 void UpdateSand(int x, int y)
 {
-    if (!SandReact(x, y))
+    if (!React(x, y, Water, Product))
         if (!Fall(x, y))
             FallIn(x, y, Water);
-}
-
-bool SandReact(int x, int y)
-{
-    if (InBounds(x, y + 1) && grid[x][y + 1] == Water)
-    {
-        grid[x][y] = Product;
-        grid[x][y + 1] = Empty;
-        return swap(&grid[x][y], &grid[x][y + 1]);
-    }
-    else if (InBounds(x, y - 1) && grid[x][y - 1] == Water)
-    {
-        grid[x][y] = Product;
-        grid[x][y - 1] = Empty;
-        return swap(&grid[x][y], &grid[x][y - 1]);
-    }
-
-    return false;
 }
 
 void UpdateProduct(int x, int y)
@@ -185,6 +211,24 @@ void UpdateProduct(int x, int y)
     if (!Fall(x, y))
         if (!FallIn(x, y, Water))
             FallIn(x, y, Sand);
+}
+
+bool React(int x, int y, CellType with, CellType to)
+{
+    if (InBounds(x, y + 1) && grid[x][y + 1] == with)
+    {
+        grid[x][y] = to;
+        grid[x][y + 1] = Empty;
+        return swap(&grid[x][y], &grid[x][y + 1]);
+    }
+    else if (InBounds(x, y - 1) && grid[x][y - 1] == with)
+    {
+        grid[x][y] = to;
+        grid[x][y - 1] = Empty;
+        return swap(&grid[x][y], &grid[x][y - 1]);
+    }
+
+    return false;
 }
 
 bool Fall(int x, int y)
@@ -202,7 +246,7 @@ bool Fall(int x, int y)
     return false;
 }
 
-bool FallIn(int x, int y, enum CellType type)
+bool FallIn(int x, int y, CellType type)
 {
     for (int distance = gravity; distance > 0; distance--)
     {
@@ -217,7 +261,9 @@ bool FallIn(int x, int y, enum CellType type)
     return false;
 }
 
-
+//----------------------------------------------------------------------------------
+// Draw everything
+//----------------------------------------------------------------------------------
 void Draw()
 {
     // Draw
@@ -225,6 +271,7 @@ void Draw()
     BeginDrawing();
         ClearBackground(RAYWHITE);
         
+        // Draw grid
         for (int x = 0; x < gridWidth; x++)
             for (int y = 0; y < gridHeight; y++)
             {
